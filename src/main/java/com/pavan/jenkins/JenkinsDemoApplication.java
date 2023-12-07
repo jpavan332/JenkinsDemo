58d3501e5fe47f426d47e4bbe8b720258ec8ce91
jpavan <jpavan332@gmail.com>
2023-12-07 11:07:17 -0600

    fisrt commit

---
+++ src/main/java/com/pavan/jenkins/JenkinsDemoApplication.java
@@ -0,0 +1,26 @@
+package com.pavan.jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+	
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started.....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed......");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavan/jenkins/JenkinsDemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.pavan.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	
+	Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		
+		logger.info("Test Case Executed....");
+		
+		assertEquals(true, true);
+	}
+
+}
